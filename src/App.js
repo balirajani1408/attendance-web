51c63ec67a13ef461076c25450317eb6a0e74f43
kiranmai kancharla <balirajani1408@gmail.com>
2023-08-04 22:39:36 +0530

    Checking user.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <h1>Attendance Web App</h1>
+        <h1>Attendance Web App!</h1>
       </header>
     </div>
   );
