725b18d1b20e420a7c52d8559854197772c9f69e
balirajani1408 <balirajani1408@gmail.com>
2023-08-05 08:47:25 +0530

    Errorn in deploying with aws.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import { NavBar } from "./components/navBar";
-import { Main } from "./components/main";
-import Footer from "./components/footer";
+// import { NavBar } from "./components/navBar";
+// import { Main } from "./components/main";
+// import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Main />
-      <Footer />
+      <header>
+        <h1>Attendance App</h1>
+      </header>
     </div>
   );
 }
